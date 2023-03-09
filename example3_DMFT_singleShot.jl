a708be04fe7eb5dbd67d2ddedca297d4482a38d7
Julian Stobbe <Atomtomate@gmx.de>
2023-03-09 12:05:26 +0100

    added example3

---
+++ example3_DMFT_singleShot.jl
@@ -0,0 +1,34 @@
+using Pkg
+Pkg.activate(@__DIR__)
+using jED
+using TimerOutputs
+
+
+ϵₖ = [1.0, 0.5, -1.1, -0.6]
+Vₖ = [0.25, 0.35, 0.45, 0.55]
+p  = AIMParams(ϵₖ, Vₖ)
+μ  = 0.6
+U  = 1.2
+β  = 13.4
+tsc = -0.40824829046386307/2
+Nν  = 1000
+Nk  = 2
+
+kG = jED.gen_kGrid("3Dsc-$tsc", Nk)
+
+νnGrid = jED.OffsetVector([1im * (2*n+1)*π/β for n in 0:Nν-1], 0:Nν-1)
+#Δi     = Δ_AIM(νnGrid, p)
+G0W    = GWeiss(νnGrid, μ, p)
+
+basis = jED.Basis(5);
+    
+model    = AIM(ϵₖ, Vₖ, μ, U)
+es       = Eigenspace(model, basis);
+GImp_i   = calc_GF_1(basis, es, νnGrid, β)
+ΣImp_i   = Σ_from_GImp(G0W, GImp_i)
+
+GLoc_i   = GLoc(ΣImp_i, μ, νnGrid, kG)
+G0W = GWeiss_from_Imp(GLoc_i, ΣImp_i)
+fit_AIM_params!(p, G0W, μ, νnGrid)
+display(p)
+GImp_old = deepcopy(GImp_i)
