33782fc5299ccc80b8f2eb8a143aa04568b3adeb
Julian Stobbe <a@b.c>
2024-07-15 13:29:38 +0000

    Hubbard Dimer example, ML script change

--- scripts/example18_HubbardDimer.jl
+++ scripts/example18_HubbardDimer.jl
@@ -4,21 +4,21 @@ using jED
 using TimerOutputs
 to = TimerOutput()
 
-β  = 19.4
+β  = 10.0 #19.4
 
-t = 2.0
-U = 10.0
+t = -1.0
+U = 2.0
 
-tMatrix = [0.0 -t
-           -t 0.0]
+tMatrix = [0.0 t
+           t 0.0]
 UMatrix = [U   0.0
            0.0 U]
-basis = jED.Basis(2, N_filter=[2], S_filter=[0])
+basis = jED.Basis(2)#, N_filter=[2], S_filter=[0])
 model = Hubbard(tMatrix, UMatrix)
 es = Eigenspace(model, basis);
 
-#νnGrid = jED.OffsetVector([1im * (2*n+1)*π/β for n in 0:2000], 0:2000)
-#GF, dens = calc_GF_1(basis, es, νnGrid, β, ϵ_cut=0.0, with_density=true)
+νnGrid = jED.OffsetVector([1im * (2*n+1)*π/β for n in 0:2000], 0:2000)
+GF, dens = calc_GF_1(basis, es, νnGrid, β, ϵ_cut=0.0, with_density=true)
 #GF_approx, dens_approx = calc_GF_1(basis, es, νnGrid, β, ϵ_cut=1e-8, with_density=true)
 
 Z = calc_Z(es, β)
